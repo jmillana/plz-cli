cee56d3010dcf15fd6eb873037fba805c30913ba
jmillana <jordi@marimon.tech>
2023-07-10 19:00:53 +0200

    ♻️ Refactor: Update imports and struct fields in completions.rs
    - Import `question::{Answer, Question}` and use it in the code
    - Update the type of `system_prompt` field in the `ChatCompletions` struct to `prompts::Prompt`
    - Update the initialization of `system_prompt` field in the `ChatCompletions` struct
    - Update the parameters and return type of the `run` method in `ChatCompletions`
    - Add a new method `refine_loop` to handle refining prompts
    - Update the `run` method to accept a vector of prompts and return a `prompts::Prompt`

--- src/chat_gpt/completions.rs
+++ src/chat_gpt/completions.rs
@@ -1,13 +1,14 @@
 use colored::Colorize;
+use question::{Answer, Question};
 use reqwest::blocking::{Client, Response};
 use serde_json::json;
-use spinners::Spinner;
+use spinners::{Spinner, Spinners};
 
 use crate::prompts;
 use crate::{Cli, Config, Mode};
 
 pub struct ChatCompletions {
-    system_prompt: String,
+    system_prompt: prompts::Prompt,
     pub cli: Cli,
     pub config: Config,
     pub mode: Mode,
@@ -21,7 +22,7 @@ impl ChatCompletions {
             _ => Mode::Command,
         };
         Self {
-            system_prompt: "".to_string(),
+            system_prompt: prompts::Prompt::new(),
             cli,
             config,
             mode,
@@ -32,10 +33,52 @@ impl ChatCompletions {
         self.system_prompt = system_prompt.prompt(&self.cli);
     }
 
-    pub fn run(self: &Self, prompt: String, spinner: &mut Spinner) -> String {
+    pub fn refine_loop(self: &Self, prompt: prompts::Prompt, spinner: &mut Spinner) -> String {
+        let mut refined_prompts = vec![prompt.clone()];
+        let mut response_prompt = self.run(refined_prompts.clone(), spinner);
+        spinner.stop_and_persist(
+            "✔".green().to_string().as_str(),
+            "Got some results!".green().to_string(),
+        );
+        let mut finish = false;
+        while !finish {
+            crate::pprint(&response_prompt.content, "bash");
+            let should_refine =
+                crate::ask_for_confirmation(">> Refine the prompt? [y/N]", Some(Answer::NO));
+            refined_prompts.push(response_prompt.clone());
+            if should_refine {
+                let user_refinement = Question::new(">> Enter your refinement: ").ask();
+                let mut spinner = Spinner::new(Spinners::BouncingBar, "Refining...".into());
+                let new_prompt = match user_refinement.unwrap() {
+                    Answer::RESPONSE(value) => prompts::Prompt::build("user".to_string(), value),
+                    _ => {
+                        println!("Invalid response");
+                        std::process::exit(1);
+                    }
+                };
+                refined_prompts.push(new_prompt);
+                response_prompt = self.run(refined_prompts.clone(), &mut spinner);
+                spinner.stop_and_persist(
+                    "✔".green().to_string().as_str(),
+                    "Refined result!".green().to_string(),
+                );
+            } else {
+                finish = true;
+            }
+        }
+        return response_prompt.content;
+    }
+
+    pub fn run(
+        self: &Self,
+        prompt: Vec<prompts::Prompt>,
+        spinner: &mut Spinner,
+    ) -> prompts::Prompt {
         let client = Client::new();
         let api_addr = format!("{}/chat/completions", self.config.api_base);
         let max_tokens = self.cli.token_limit.unwrap_or(self.config.max_tokens);
+        let mut group_prompts = vec![self.system_prompt.clone()];
+        group_prompts.extend(prompt.clone());
 
         let response = client
             .post(api_addr)
@@ -46,24 +89,28 @@ impl ChatCompletions {
                 "presence_penalty": 0,
                 "frequency_penalty": 0,
                 "model": "gpt-3.5-turbo",
-                "messages": [
-                    {"role": "system", "content": self.system_prompt},
-                    {"role": "user", "content": prompt}
-                ],
+                "messages": group_prompts,
             }))
             .header("Authorization", format!("Bearer {}", &self.config.api_key))
             .send()
             .unwrap();
 
         let validated_response = self.validate_response(response, spinner);
-        let response_string = validated_response.json::<serde_json::Value>().unwrap()["choices"][0]
-            ["message"]["content"]
-            .as_str()
-            .unwrap()
-            .trim()
-            .to_string();
+        let message =
+            serde_json::from_str::<serde_json::Value>(&validated_response.text().unwrap()).unwrap();
 
-        return response_string;
+        let response_prompt = prompts::Prompt::build(
+            message["choices"][0]["message"]["role"]
+                .as_str()
+                .unwrap()
+                .to_string(),
+            message["choices"][0]["message"]["content"]
+                .as_str()
+                .unwrap()
+                .to_string(),
+        );
+
+        return response_prompt;
     }
 
     fn validate_response(self: &Self, response: Response, spinner: &mut Spinner) -> Response {
